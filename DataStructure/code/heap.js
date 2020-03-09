e26c0936c86a536583700185f1c6d34deabf5a09
AddJunZ <1574118221@qq.com>
2020-03-09 11:25:59 +0800

    寻找最小k个数字的最大堆实现

--- DataStructure/code/heap.js
+++ DataStructure/code/heap.js
@@ -1,8 +1,3 @@
-// 1.直接排序
-// 2.最大堆
-// 3.快排思想
-
-// 2.最大堆
 // 最大堆
 class MaxHeap {
   constructor(arr = []) {
@@ -69,59 +64,3 @@ class MaxHeap {
 function swap(arr, i, j) {
   [arr[i], arr[j]] = [arr[j], arr[i]];
 }
-
-function kMin(arr, k){
-  const length = arr.length;
-  if(k >= length){
-    return arr;
-  }
-
-  const heap = new MaxHeap(arr.slice(0,k));
-  for(let i = k; i < length; ++i){
-    if(heap.top() > arr[i]){
-      heap.extract();
-      heap.insert(arr[i]);
-    }
-  }
-  return heap.container;
-}
-
-// 3.快排思想
-// function patition(arr, start, end){
-//   var temp = arr[start];
-//   var i = start;
-//   var j = end;
-//   while(i < j){
-//     while(i < j && arr[j] > temp){
-//       j--
-//     }
-//     while(i < j && arr[i] <= temp){
-//       i++;
-//     }
-//     if(i < j){
-//       [arr[i], arr[j]] = [arr[j], arr[i]];
-//     }
-//   }
-//   [arr[start], arr[i]] = [arr[i], arr[start]];
-//   return i;
-// }
-
-// function kMin(arr, k){
-//   const len = arr.length;
-//   if(k >= len)return arr;
-//   let left = 0;
-//   let right = len - 1;
-//   let index = patition(arr, left, right);
-//   while(index !== k){
-//     if(index < k){
-//       left = index + 1;
-//       index = patition(arr, left, right);
-//     }else if(index > k){
-//       right = index - 1;
-//       index = patition(arr, left, right);
-//     }
-//   }
-//   return arr.slice(0, k);
-// }
-console.log(kMin([1,6,2,6,1,-1,2,3,8,2],4));
-
