a515ec24addb867c536ed6a1ae447130cbf0933c
AddJunZ <15018376053@163.com>
2020-09-14 17:18:32 +0800

    feat: 链表相交

--- LeetCode/程序员面试金典/code/16.js
+++ LeetCode/程序员面试金典/code/16.js
@@ -12,6 +12,35 @@
  * @param {ListNode} headB
  * @return {ListNode}
  */
+// 1. 这道题和leetcode52很像，先计算两个链表的长度，将长的链表先向前移动两者长度的差值
+// 再将两个链表进行遍历，能访问到同一个链表节点就是共同节点
 var getIntersectionNode = function(headA, headB) {
-    
+  let lenA = 0, lenB = 0, p = headA, q = headB;
+  while(p){
+    lenA++;
+    p = p.next;
+  }
+  while(q){
+    lenB++;
+    q = q.next;
+  }
+  let dis = Math.abs(lenA - lenB);
+  if(lenA > lenB){
+    while(dis--){
+      headA = headA.next;
+    }
+  }
+  if(lenA < lenB){
+    while(dis--){
+      headB = headB.next;
+    }
+  }
+  while(headA){
+    if(headA == headB){
+      return headA;
+    }
+    headA = headA.next;
+    headB = headB.next;
+  }
+  return null;
 };
---
+++ LeetCode/程序员面试金典/code/17.js
@@ -0,0 +1,16 @@
+// 环路检测
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    
+};
