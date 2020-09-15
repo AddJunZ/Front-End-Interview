1c9650f025fdad65f0397bc5dbfb531966cae23b
AddJunZ <15018376053@163.com>
2020-09-15 19:34:59 +0800

    feat: 堆盘子。。

--- LeetCode/程序员面试金典/code/20.js
+++ LeetCode/程序员面试金典/code/20.js
@@ -1,9 +1,12 @@
 // 堆盘子
+// 实现一个栈数组。每个栈的大小有限制
+// 要求对栈数组的操作如同只有一个栈的效果
 /**
- * @param {number} cap
+ * @param {number} cap 每个栈的容量
  */
 var StackOfPlates = function(cap) {
-
+  this.cap = cap;
+  this.stackList = [];
 };
 
 /** 
@@ -11,14 +14,27 @@ var StackOfPlates = function(cap) {
  * @return {void}
  */
 StackOfPlates.prototype.push = function(val) {
-
+  if(this.cap <= 0)return ;
+  // 如果还有位置
+  if(this.stackList.length > 0 && this.stackList[this.stackList.length - 1].length < this.cap){
+    this.stackList[this.stackList.length - 1].push(val);
+  }else{
+    this.stackList.push([val]);
+  }
 };
 
 /**
  * @return {number}
  */
 StackOfPlates.prototype.pop = function() {
-
+  if(this.stackList.length > 0 && this.stackList[this.stackList.length - 1].length === 1){
+    let endArr = this.stackList.pop();
+    return endArr.pop();
+  }else{
+    return this.stackList.length > 0 ? 
+      this.stackList[this.stackList.length - 1].pop() :
+      -1;
+  }
 };
 
 /** 
@@ -26,7 +42,12 @@ StackOfPlates.prototype.pop = function() {
  * @return {number}
  */
 StackOfPlates.prototype.popAt = function(index) {
-
+  if(!this.stackList[index])return -1;
+  if(this.stackList[index].length === 1){
+    return this.stackList.splice(index, 1);
+  }else{
+    return this.stackList[index].pop();
+  }
 };
 
 /**
