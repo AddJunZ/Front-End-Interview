4171371e32c9d90691af61ecd2cedf6893fbd1bf
AddJunZ <15018376053@163.com>
2020-09-21 13:37:03 +0800

    feat: 补两张图

--- LeetCode/程序员面试金典/code/29.js
+++ LeetCode/程序员面试金典/code/29.js
@@ -11,19 +11,3 @@
  * @param {TreeNode} p
  * @return {TreeNode}
  */
-var inorderSuccessor = function(root, p) {
-  let bool = false;
-  function inorderDFS(r){
-    if(!r)return ;
-    inorderDFS(r.left);
-    if(bool){
-      return r;
-    }
-    if(r === p){
-      bool = true
-    }
-    inorderDFS(r.right);
-  }
-  inorderDFS(root);
-  return null;
-};
