633a578eef7801c6cfdc830ee1bf5a1fa3122e96
AddJunZ <15018376053@163.com>
2020-12-17 23:51:05 +0800

    feat: 会个锤子shell，还想看expect

---
+++ shell/test/example01.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#This is to show what a example looks like.
+echo "Our first example"
+echo # This inserts an empty line in output.
+echo "We are currently in the following directory."
+pwd
+echo
+echo "This directory contains the following files"
+ls
