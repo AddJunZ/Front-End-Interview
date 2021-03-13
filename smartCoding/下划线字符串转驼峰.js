e3922b442fc789a68eadc44470a73b01c9d1461c
AddJunZ <15018376053@163.com>
2021-03-13 17:54:42 +0800

    feat: 下划线转驼峰优化

--- smartCoding/下划线字符串转驼峰.js
+++ smartCoding/下划线字符串转驼峰.js
@@ -1,8 +1,11 @@
+// 1. 常规做法
 const TurnToHump = (underlineStr) => {
   return underlineStr.split('_').filter(x => x).map((word, index) => {
-    if (index === 0) return word.toLowerCase();
-    const tail = word.slice(1).toLowerCase();
-    return word[0].toUpperCase() + tail; 
+    word = word.toLowerCase();
+    if (index === 0) return word;
+    const tail = word.slice(1);
+    return word[0].toUpperCase() + tail;
   }).join("");
 }
-console.log(TurnToHump('_world__hello_'));
+
+console.log(TurnToHump('_world__he_allo_'));
