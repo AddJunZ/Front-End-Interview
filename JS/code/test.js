bf8c9898f511dcde7706cf164064f15e4b84044d
AddJunZ <15018376053@163.com>
2021-03-17 14:44:37 +0800

    feat: 反转链表

--- JS/code/test.js
+++ JS/code/test.js
@@ -1,43 +1,16 @@
-function multiRequest(urls = [], maxNum) {
-  // 总请求数
-  const len = urls.length;
-  // 请求结果数组
-  const result = new Array(len).fill(false);
-  // 当前完成的请求数，也是下一次发送请求的index，即urls[index]
-  let count = 0;
-  return new Promise((resolve, reject) => {
-    // 1. 直接请求maxNum个异步请求
-    while (count < maxNum) {
-      next();
+function test(a,b) {
+  console.log(b)
+  return {
+    test:function(c){
+      return test(c,a);
     }
-
-    // 2. 发送一次异步请求
-    function next() {
-      // 3. 当前的异步任务序列
-      const index = count++;
-      // 4. 边界：完成所有任务
-      if (index >= len) {
-        !result.includes(false) && resolve(result);
-        return ;
-      }
-
-      const url = urls[index];
-      // 5. 请求接口，在每个请求结束后都可以继续请求下一个
-      fetch(url)
-        .then(res => {
-          result[index] = res;
-        })
-        .catch(err => {
-          result[index] = err;
-        })
-        .finally(() => {
-          // 请求没有全部完成，递归
-          if (index < len) {
-            next();
-          }
-        })
-    }
-  })
+  };
 }
-
-multiRequest(['http://localhost:3000','http://localhost:3000','http://localhost:3000','http://localhost:3000','http://localhost:3000','http://localhost:3000','http://localhost:3000'], 3);
+var retA = test(0); // undefined
+retA.test(2);// 0
+ retA.test(4);// 0
+ retA.test(8);// 0
+var retB = test(0).test(2).test(4).test(8);// undefined 0 2 4
+var retC = test('good').test('bad'); // undefined good 
+retC.test('good');// bad
+ retC.test('bad');// bad
