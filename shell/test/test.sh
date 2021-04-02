89ec640af6e2818c9922d543e94c1d92d7b9c81f
AddJunZ <15018376053@163.com>
2021-04-03 01:01:32 +0800

    feat: str equal

---
+++ shell/test/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 1.
+# num1=12
+# num2=13
+# if [ $num1 -le $num2 ]
+# then 
+#   echo "yes"
+# else
+#   echo "no"
+# fi
+
+# yes
+str1="abc"
+str2="abc"
+if [ "$str1" == "$str2" ]
+then 
+  echo "yes"
+else
+  echo "no"
+fi
