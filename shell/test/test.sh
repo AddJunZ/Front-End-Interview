fcf23b57619dfb9ce3adcdb927b07c4eb33d8064
AddJunZ <15018376053@163.com>
2021-05-09 19:09:37 +0800

    feat: shell的%和#

--- shell/test/test.sh
+++ shell/test/test.sh
@@ -1,4 +1,13 @@
-number="12345"
-echo ${number#} # 12345
-echo ${number#?} # 2345
-echo ${number%${number#?}} # 1
+# number="12345"
+# echo ${number#} # 12345
+# echo ${number#?} # 2345
+# echo ${number%${number#?}} # 1
+# t="345"
+# echo ${number%${t}}
+str="12345"
+t="345"
+k="123"
+echo ${str%$t} # 12
+echo ${str%$k} # 12345
+echo ${str#$t} # 12345
+echo ${str#$k} # 45
