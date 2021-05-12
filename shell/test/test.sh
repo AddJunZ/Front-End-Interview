97dd08b7940ca8e7c7fd0ed9e97f59b2928e0fb6
AddJunZ <15018376053@163.com>
2021-05-12 15:11:32 +0800

    feat: shell

--- shell/test/test.sh
+++ shell/test/test.sh
@@ -1,13 +1,5 @@
-# number="12345"
-# echo ${number#} # 12345
-# echo ${number#?} # 2345
-# echo ${number%${number#?}} # 1
-# t="345"
-# echo ${number%${t}}
-str="12345"
-t="345"
-k="123"
-echo ${str%$t} # 12
-echo ${str%$k} # 12345
-echo ${str#$t} # 12345
-echo ${str#$k} # 45
+1
+2
+3
+4
+123512
