05f83e3a89655c1046712137ed695cb2c8d66329
AddJunZ <15018376053@163.com>
2022-04-05 14:50:28 +0800

    feat: iframe both-way communication

--- HTML/code/iframe/constants.js
+++ HTML/code/iframe/constants.js
@@ -1,6 +1,7 @@
 // constants
 const actions = {
   videoInfo: 'videoInfo',
+  jumpVideo: 'jumpVideo'
 };
 // can't use localhost if u serve your html in 127.0.0.1
 // the origin should be the same as where your server serve at.
